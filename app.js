2dd0348482fc805c38789f64aded9873dd184d6e
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-14 14:18:13 -0400

    flight search backend complete

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const unirest = require('unirest');
 const inquirer = require('inquirer');
+const moment = require('moment');
+const momentDurationFormatSetup = require('moment-duration-format');
+
+momentDurationFormatSetup(moment);
 
 let req = unirest(
   'GET',
@@ -8,72 +12,155 @@ let req = unirest(
 
 console.clear();
 
-inquirer
-  .prompt([
-    {
-      name: 'origin',
-      type: 'input',
-      message: 'Origin (airport abbreviation):'
-    },
-    {
-      name: 'destination',
-      type: 'input',
-      message: 'Destination (airport abbreviation)'
-    },
-    {
-      name: 'departdate',
-      type: 'input',
-      message: 'Depart Date (YYYY-MM-DD)'
+const start = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'startchoice',
+        type: 'list',
+        message: 'Welcome:',
+        choices: ['Search Flights', 'Search Hotels']
+      }
+    ])
+    .then(answer => {
+      answer.startchoice === 'Search Flights' ? flightSearch() : hotelSearch();
+    });
+};
+
+const flightSearch = () => {
+  console.clear();
+  inquirer
+    .prompt([
+      {
+        name: 'origin',
+        type: 'input',
+        message: 'Origin (airport abbreviation):'
+      },
+      {
+        name: 'destination',
+        type: 'input',
+        message: 'Destination (airport abbreviation)'
+      },
+      {
+        name: 'departdate',
+        type: 'input',
+        message: 'Depart Date (YYYY-MM-DD)'
+      }
+    ])
+    .then(answers => {
+      console.log('Finding flights...');
+      const searchOrigin = answers.origin;
+      const searchDest = answers.destination;
+      const searchDepDate = answers.departdate;
+
+      req.query({
+        origin1: searchOrigin,
+        destination1: searchDest,
+        departdate1: searchDepDate,
+        cabin: 'e',
+        currency: 'USD',
+        adults: '1',
+        bags: '0'
+      });
+
+      req.headers({
+        'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+        'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+      });
+
+      req.end(function(res) {
+        if (res.error) throw new Error(res.error);
+
+        const results = res.body;
+
+        displayResults(results);
+      });
+    });
+
+  const displayResults = results => {
+    console.clear();
+
+    for (i = 0; i <= 4; i++) {
+      let trip = results.tripset[Object.keys(results.tripset)[i]];
+
+      let totalDuration = moment
+        .duration(trip.duration, 'minutes')
+        .format('h [hours] mm [minutes]');
+
+      console.log(`
+        Flight ${i + 1} | Duration: ${totalDuration}
+        ---------------------------------------
+    `);
+
+      let legs = trip.legs;
+
+      legs[0].segments.forEach((segment, sI) => {
+        let flightSegment = results.segset[segment];
+
+        let airlineCode = flightSegment.airlineCode;
+        let airline = results.airlines[airlineCode];
+        let flightNumber = flightSegment.flightNumber;
+        let originCode = flightSegment.originCode;
+        let origin = results.airportDetails[originCode].name;
+        let originCity = results.airportDetails[originCode].city;
+        let destinationCode = flightSegment.destinationCode;
+        let destination = results.airportDetails[destinationCode].name;
+        let destinationCity = results.airportDetails[destinationCode].city;
+        let leaveTime = flightSegment.leaveTimeDisplay;
+        let arriveTime = flightSegment.arriveTimeDisplay;
+        let duration = moment
+          .duration(flightSegment.duration, 'minutes')
+          .format('h [hours] mm [minutes]');
+
+        console.log(`
+        Segment ${sI + 1}: 
+        
+        Airline: ${airline}
+        FlightNumber: ${airlineCode}-${flightNumber}
+
+        Origin: ${origin}
+        Destination: ${destination}
+        
+        Departure time in ${origin}: ${leaveTime}
+        Arrival time in ${destinationCity}: ${arriveTime}
+
+        Duration: ${duration}
+      `);
+      });
+      console.log(`
+        ---------------------------------------
+    `);
     }
-  ])
-  .then(answers => {
-    console.log('Finding flights...');
-    const searchOrigin = answers.origin;
-    const searchDest = answers.destination;
-    const searchDepDate = answers.departdate;
+  };
+};
 
-    req.query({
-      origin1: searchOrigin,
-      destination1: searchDest,
-      departdate1: searchDepDate,
-      cabin: 'e',
-      currency: 'USD',
-      adults: '1',
-      bags: '0'
-    });
-
-    req.headers({
-      'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
-      'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
-    });
-
-    req.end(function(res) {
-      if (res.error) throw new Error(res.error);
-
-      const results = res.body;
-
-      displayResults(results.segset[Object.keys(results.segset)[0]]);
-    });
-  });
-
-const displayResults = results => {
+const hotelSearch = () => {
   console.clear();
 
-  const airline = results.airlineCode;
-  const flightNumber = `${airline}-${results.flightNumber}`;
-  const originCode = results.originCode;
-  const destCode = results.destinationCode;
-  const leaveTime = results.leaveTimeDisplay;
-  const arriveTime = results.arriveTimeDisplay;
+  var req = unirest(
+    'GET',
+    'https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session'
+  );
 
-  console.log(`
-    Your Flight:
+  req.query({
+    airportcode: 'HAN',
+    rooms: '1',
+    citycode: '42700',
+    checkin: '2018-12-20',
+    checkout: '2018-12-24',
+    adults: '1'
+  });
 
-    Airline: ${airline}
-    Flight Number: ${flightNumber}
-    Origin Airport: ${originCode}
-    Destination Airport: ${destCode}
-    Leave Time: ${leaveTime}
-    Arrive Time: ${arriveTime}
-  `);
+  req.headers({
+    'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+  });
+
+  req.end(function(res) {
+    if (res.error) throw new Error(res.error);
+
+    console.log(res.body);
+  });
 };
+
+start();
