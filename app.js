8697af3a87e310890686e993c646bea401f56822
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-10-15 20:26:10 -0400

    search for hotels and flights together

--- app.js
+++ app.js
@@ -8,111 +8,225 @@ momentDurationFormatSetup(moment);
 console.clear();
 
 const start = () => {
+  console.log(`
+      my concierge`);
   inquirer
     .prompt([
       {
-        name: 'startchoice',
-        type: 'list',
-        message: 'Welcome:',
-        choices: ['Search Flights', 'Search Hotels']
-      }
-    ])
-    .then(answer => {
-      answer.startchoice === 'Search Flights' ? flightSearch() : hotelSearch();
-    });
-};
-
-const flightSearch = () => {
-  console.clear();
-  inquirer
-    .prompt([
+        name: 'location',
+        type: 'input',
+        message: 'Welcome, where are you headed (City, Country)?'
+      },
       {
         name: 'origin',
         type: 'input',
-        message: 'Origin (airport abbreviation):'
+        message: 'Where are you leaving from (City, Country)?'
       },
       {
-        name: 'destination',
+        name: 'checkindate',
         type: 'input',
-        message: 'Destination (airport abbreviation)'
+        message: 'When are you leaving (YYYY-MM-DD)?'
       },
       {
-        name: 'departdate',
+        name: 'checkoutdate',
         type: 'input',
-        message: 'Depart Date (YYYY-MM-DD)'
+        message: 'When are you returning (YYYY-MM-DD)?'
       }
     ])
     .then(answers => {
-      console.log('Finding flights...');
-      const searchOrigin = answers.origin;
-      const searchDest = answers.destination;
-      const searchDepDate = answers.departdate;
-
-      let req = unirest(
-        'GET',
-        'https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session'
+      citySearch(
+        answers.location,
+        answers.origin,
+        answers.checkindate,
+        answers.checkoutdate
       );
-
-      req.query({
-        origin1: searchOrigin,
-        destination1: searchDest,
-        departdate1: searchDepDate,
-        cabin: 'e',
-        currency: 'USD',
-        adults: '1',
-        bags: '0'
-      });
-
-      req.headers({
-        'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
-        'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
-      });
-
-      req.end(function(res) {
-        if (res.error) throw new Error(res.error);
-
-        const results = res.body;
-
-        displayFlightResults(results);
-      });
     });
+};
 
-  const displayFlightResults = results => {
-    console.clear();
+const citySearch = (destination, origin, checkindate, checkoutdate) => {
+  const req1 = unirest(
+    'GET',
+    'https://apidojo-kayak-v1.p.rapidapi.com/locations/search'
+  );
 
-    for (i = 0; i <= 4; i++) {
-      let trip = results.tripset[Object.keys(results.tripset)[i]];
+  req1.query({
+    where: destination
+  });
 
-      let totalDuration = moment
-        .duration(trip.duration, 'minutes')
+  req1.headers({
+    'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+  });
+
+  const req2 = unirest(
+    'GET',
+    'https://apidojo-kayak-v1.p.rapidapi.com/locations/search'
+  );
+
+  req2.query({
+    where: origin
+  });
+
+  req2.headers({
+    'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+  });
+
+  req1.end(function(res1) {
+    if (res1.error) throw new Error(res1.error);
+
+    destCity = res1.body.filter(result => result.loctype === 'city')[0];
+    destAirport = res1.body.filter(result => result.loctype === 'ap')[0];
+
+    req2.end(function(res2) {
+      if (res2.error) throw new Error(res2.error);
+
+      originAirport = res2.body.filter(result => result.loctype === 'ap')[0];
+
+      hotelSearch(
+        destCity.ctid,
+        checkindate,
+        checkoutdate,
+        destAirport.apicode,
+        originAirport.apicode
+      );
+    });
+  });
+};
+
+const hotelSearch = (
+  cityId,
+  checkInDate,
+  checkOutDate,
+  destAirportCode,
+  originAirportCode
+) => {
+  let req = unirest(
+    'GET',
+    'https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session'
+  );
+
+  req.query({
+    rooms: '1',
+    citycode: cityId,
+    checkin: checkInDate,
+    checkout: checkOutDate,
+    adults: '1'
+  });
+
+  req.headers({
+    'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+  });
+
+  req.end(function(res) {
+    if (res.error) throw new Error(res.error);
+
+    displayHotelResults(res.body);
+    flightSearch(originAirportCode, destAirportCode, checkInDate);
+  });
+};
+
+const displayHotelResults = results => {
+  console.clear();
+
+  for (i = 0; i <= 4; i++) {
+    let hotel = results.hotelset[i];
+
+    let {
+      userrating: userRating,
+      address: hotelAddress,
+      city: hotelCity,
+      country: hotelCountry,
+      name: hotelName,
+      price: hotelPrice
+    } = hotel;
+
+    console.log(`
+      Hotel ${i + 1} | ${hotelName}
+      ---------------------------------------`);
+
+    console.log(`
+      ${hotelAddress}
+      ${hotelCity}, ${hotelCountry}
+
+      Rating: ${userRating}
+
+      Price: ${hotelPrice}
+
+      ---------------------------------------
+  `);
+  }
+};
+
+const flightSearch = (originAirportCode, destAirportCode, departDate) => {
+  console.clear();
+
+  let req = unirest(
+    'GET',
+    'https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session'
+  );
+
+  req.query({
+    origin1: originAirportCode,
+    destination1: destAirportCode,
+    departdate1: departDate,
+    cabin: 'e',
+    currency: 'USD',
+    adults: '1',
+    bags: '0'
+  });
+
+  req.headers({
+    'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
+    'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
+  });
+
+  req.end(function(res) {
+    if (res.error) throw new Error(res.error);
+
+    const results = res.body;
+
+    displayFlightResults(results);
+  });
+};
+
+const displayFlightResults = results => {
+  console.clear();
+
+  for (i = 0; i <= 4; i++) {
+    let trip = results.tripset[Object.keys(results.tripset)[i]];
+
+    let totalDuration = moment
+      .duration(trip.duration, 'minutes')
+      .format('h [hours] mm [minutes]');
+
+    let legs = trip.legs;
+
+    console.log(`
+    Flight ${i + 1} | Duration: ${totalDuration}
+    ---------------------------------------
+    `);
+
+    legs[0].segments.forEach((segment, sI) => {
+      let flightSegment = results.segset[segment];
+
+      let airlineCode = flightSegment.airlineCode;
+      let airline = results.airlines[airlineCode];
+      let flightNumber = flightSegment.flightNumber;
+      let originCode = flightSegment.originCode;
+      let origin = results.airportDetails[originCode].name;
+      let originCity = results.airportDetails[originCode].city;
+      let destinationCode = flightSegment.destinationCode;
+      let destination = results.airportDetails[destinationCode].name;
+      let destinationCity = results.airportDetails[destinationCode].city;
+      let leaveTime = flightSegment.leaveTimeDisplay;
+      let arriveTime = flightSegment.arriveTimeDisplay;
+      let duration = moment
+        .duration(flightSegment.duration, 'minutes')
         .format('h [hours] mm [minutes]');
 
-      let legs = trip.legs;
-
-      legs[0].segments.forEach((segment, sI) => {
-        let flightSegment = results.segset[segment];
-
-        let airlineCode = flightSegment.airlineCode;
-        let airline = results.airlines[airlineCode];
-        let flightNumber = flightSegment.flightNumber;
-        let originCode = flightSegment.originCode;
-        let origin = results.airportDetails[originCode].name;
-        let originCity = results.airportDetails[originCode].city;
-        let destinationCode = flightSegment.destinationCode;
-        let destination = results.airportDetails[destinationCode].name;
-        let destinationCity = results.airportDetails[destinationCode].city;
-        let leaveTime = flightSegment.leaveTimeDisplay;
-        let arriveTime = flightSegment.arriveTimeDisplay;
-        let duration = moment
-          .duration(flightSegment.duration, 'minutes')
-          .format('h [hours] mm [minutes]');
-
-        console.log(`
-        Flight ${i + 1} | Duration: ${totalDuration}
-        ---------------------------------------
-        `);
-
-        console.log(`
+      console.log(`
         Segment ${sI + 1}: 
         
         Airline: ${airline}
@@ -126,120 +240,11 @@ const flightSearch = () => {
 
         Duration: ${duration}
       `);
-      });
-      console.log(`
+    });
+    console.log(`
         ---------------------------------------
     `);
-    }
-  };
-};
-
-const hotelSearch = async () => {
-  console.clear();
-
-  await inquirer
-    .prompt([
-      {
-        name: 'city',
-        type: 'input',
-        message: 'City:'
-      },
-      {
-        name: 'checkindate',
-        type: 'input',
-        message: 'Check-in Date:'
-      },
-      {
-        name: 'checkoutdate',
-        type: 'input',
-        message: 'Check-out Date:'
-      }
-    ])
-    .then(answers => {
-      const req1 = unirest(
-        'GET',
-        'https://apidojo-kayak-v1.p.rapidapi.com/locations/search'
-      );
-
-      req1.query({
-        where: answers.city
-      });
-
-      req1.headers({
-        'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
-        'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
-      });
-
-      req1.end(function(res) {
-        if (res.error) throw new Error(res.error);
-        hotelCitySearch(
-          res.body[0].ctid,
-          answers.checkindate,
-          answers.checkoutdate
-        );
-      });
-    });
-
-  const hotelCitySearch = (cityId, checkInDate, checkOutDate) => {
-    let req2 = unirest(
-      'GET',
-      'https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session'
-    );
-
-    req2.query({
-      rooms: '1',
-      citycode: cityId,
-      checkin: checkInDate,
-      checkout: checkOutDate,
-      adults: '1'
-    });
-
-    req2.headers({
-      'x-rapidapi-host': 'apidojo-kayak-v1.p.rapidapi.com',
-      'x-rapidapi-key': '2f3a0e5559mshf0b9a7a94324ff7p1bf4dajsna06e1715474c'
-    });
-
-    req2.end(function(res) {
-      if (res.error) throw new Error(res.error);
-
-      displayHotelResults(res.body);
-    });
-  };
-
-  const displayHotelResults = results => {
-    console.clear();
-
-    for (i = 0; i <= 4; i++) {
-      let hotel = results.hotelset[i];
-
-      let {
-        name: hotelName,
-        displayprice: hotelPrice
-      } = hotel.cheapestProvider;
-
-      let {
-        userrating: userRating,
-        address: hotelAddress,
-        city: hotelCity,
-        country: hotelCountry
-      } = hotel;
-
-      console.log(`
-        Hotel ${i + 1} | ${hotelName}
-        ---------------------------------------`);
-
-      console.log(`
-        ${hotelAddress}
-        ${hotelCity}, ${hotelCountry}
-
-        Rating: ${userRating}
-
-        Price: ${hotelPrice}
-
-        ---------------------------------------
-    `);
-    }
-  };
+  }
 };
 
 start();
